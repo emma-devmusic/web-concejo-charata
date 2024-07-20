4a54ace332e5abb2b8a2d9e1e6256ad487d1d4d9
Emma Araujo <ema.araujo.ea@gmail.com>
2024-07-20 13:36:15 -0300

    changes enviroments

--- src/app/informe-de-sesiones/page.tsx
+++ src/app/informe-de-sesiones/page.tsx
@@ -3,7 +3,7 @@ import Shares from "../views/Shares";
 
 export default async function InformeSesionesPage() {
     
-    const resp = await fetch('http://localhost:3000/api/sessions', { next: { revalidate: 60 } })
+    const resp = await fetch(`${process.env.BASE_URL}api/sessions`, { next: { revalidate: 60 } })
     const blogs = await resp.json()
 
     return <Shares section="Noticias - Blogs" title="Enterate De Todas Las Participaciones del Concejo" blogs={blogs} />
--- src/app/shares/page.tsx
+++ src/app/shares/page.tsx
@@ -5,7 +5,7 @@ import Shares from "../views/Shares";
 
 export default async function SharesPage() {
 
-    const resp = await fetch('http://localhost:3000/api/blogs', { next: { revalidate: 60 } })
+    const resp = await fetch(`${process.env.BASE_URL}api/blogs`, { next: { revalidate: 60 } })
     const blogs = await resp.json()
 
     return <Shares section="Noticias - Blogs" title="Enterate De Todas Las Participaciones del Concejo" blogs={blogs}/>
