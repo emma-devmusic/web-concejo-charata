c3e8542a747bb64e66d87edd5660c043d2bdc8ba
Emma Araujo <ema.araujo.ea@gmail.com>
2024-07-24 11:54:30 -0300

    changes banner and information sessions

--- src/app/informe-de-sesiones/page.tsx
+++ src/app/informe-de-sesiones/page.tsx
@@ -1,10 +1,21 @@
+'use client'
 
+import { useEffect, useState } from "react";
 import Shares from "../views/Shares";
+import { Blog } from "../types";
 
-export default async function InformeSesionesPage() {
+export default function InformeSesionesPage() {
     
-    const resp = await fetch(`${process.env.BASE_URL}api/sessions`, { next: { revalidate: 60 } })
-    const blogs = await resp.json()
+    const [isLoading, setIsLoading] = useState(false)
+    const [blogs, setBlogs] = useState<Blog[]>([])
 
-    return <Shares section="Noticias - Blogs" title="Enterate De Todas Las Participaciones del Concejo" blogs={blogs} />
+    useEffect( () => {
+        setIsLoading(true)
+        fetch('/api/sessions')
+        .then(resp => resp.json())
+        .then(data => setBlogs(data))
+        .finally(() => setIsLoading(false))
+    }, [])
+
+    return <Shares section="Noticias - Blogs" title="Enterate De Todas Las Participaciones del Concejo" blogs={blogs} isLoading={isLoading}/>
 }
